c6696d4193fbdf54ce429205e129a639bb6a7425
Brandon Molyneaux <bdm0041@auburn.edu>
2021-05-16 17:56:59 -0500

    updated yaml to include sphinx.

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 
-from distutils.core import setup
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
   name = 'nwsapy',         # How you named your package folder (MyLib)
-  packages = ['nwsapy'],   # Chose the same as "name"
+  packages = ['src'],   # Chose the same as "name"
+  long_description=README,
+  long_description_content_type="text/markdown",
   version = '0.0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A package to help with NWS API requests',   # Give a short description about your library
@@ -17,6 +26,7 @@ setup(
           'pandas',
           'numpy',
       ],
+  python_requires = '>=3.8',
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
