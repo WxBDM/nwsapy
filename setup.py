1ad18b922f59cbf8e1aab38c26b0ad1bbec34fd2
Brandon M <bdmolyne@gmail.com>
2022-01-02 20:56:50 -0500

    fixed dependency critical errors and updated setup.py classifiers

--- setup.py
+++ setup.py
@@ -35,8 +35,8 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: Apache 2.0 License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      # Specify which python versions that you want to support
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3', 
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
   ],
