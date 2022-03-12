ac6ecda395dceb3bbfec5934b91e9ffa86fab10c
Brandon M <bdmolyne@gmail.com>
2022-03-12 13:13:49 -0500

    updated setup.py for a new version.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
-version = "1.0.2"
+version = "1.0.3"
 
 setup(
   name = 'nwsapy',         # How you named your package folder (MyLib)
